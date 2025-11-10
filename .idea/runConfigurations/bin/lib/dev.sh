23f683b34a0bf0e1c01303a0bbd4b2c783111306
C. M. de Picciotto <d3p1@d3p1.dev>
2025-11-10 23:30:24 +0000

    feat: testing server components [#1]

--- .idea/runConfigurations/bin/lib/dev.sh
+++ .idea/runConfigurations/bin/lib/dev.sh
@@ -11,7 +11,17 @@
 # @return void
 ##
 main() {
-    docker compose run --rm -it -v ~/.ssh/:/home/node/.ssh/ -v ~/.gitconfig:/home/node/.gitconfig cli /bin/bash
+    ##
+    # @note It is shared Git configuration (like author information),
+    #       to avoid having to define it again in the container
+    #       while using Git
+    # @note It is mounted the SSH agent socket (`$SSH_AUTH_SOCK`)
+    #       to the container. In that way, we can use it to
+    #       execute SSH connections to our trusted hosts
+    #       without sharing our private keys.
+    #       This is useful to work with SSH repositories
+    ##
+    docker compose run --rm -it -v ~/.gitconfig:/home/node/.gitconfig -v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent cli /bin/bash
 }
 
 ##
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,25 @@
  * @description Layout
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import React from "react";
-import type { Metadata } from "next";
-import "./globals.css";
+import React from 'react'
+import type {Metadata} from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Next.js | Playground",
-  description: "A playground for Next.js",
-};
+  title: 'Next.js | Playground',
+  description: 'A playground for Next.js',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="w-screen h-screen bg-neutral-950 text-white grid place-items-center">
         {children}
       </body>
     </html>
-  );
+  )
 }
