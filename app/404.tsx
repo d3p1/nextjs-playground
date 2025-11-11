01e53f5df123d4578061190136f37fbe097d741f
C. M. de Picciotto <d3p1@d3p1.dev>
2025-11-11 00:25:04 +0000

    feat: add error pages [#1]

---
+++ app/404.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description Not found page
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default function PageNotFound() {
+  return <p>Page not found.</p>
+}
---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+/**
+ * @description Error page
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+'use client'
+
+export default function ErrorPage() {
+  return <p>Error</p>
+}
